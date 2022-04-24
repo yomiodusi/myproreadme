f662958448fe5ee97621268576a0b88730b3bbb5
yomiodusi <99558274+yomiodusi@users.noreply.github.com>
2022-04-24 10:51:19 -0400

    added read me questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,56 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+        type: "input",
+        message: "What is the name of your project?",
+        name: "Title"
+    }, {
+        type: "input",
+        message: "What the goal of this project? Give a detailed output of what you want to acomplish with this project?",
+        name: "Description"
+    }, {
+        type: "input",
+        message: "Provide a quick visual of your project",
+        name: "Visuals"
+    }, {
+        type: "input",
+        message: "What is required for this project to be installed?",
+        name: "Requirements"
+    }, {
+        type: "input",
+        message: "How is this project run? Give instructions",
+        name: "Usage"
+    }, {
+        type: "input",
+        message: "Who contributed to this project?:",
+        name: "Contributing"
+    }, {
+        type: "input",
+        message: "What commands are needed to test this app?",
+        name: "Tests"
+    }, {
+        type: "input",
+        message: "Do you have any ideas for future releases?",
+        name: "Roadmap"
+    }, {
+        type: "input",
+        message: "Provide your contact information for inquries.",
+        name: "Questions"
+    }, {
+        type: 'input',
+        message: 'What is your Github username?',
+        name: 'Username'
+    }, {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'Email'
+    }, {
+        type: 'input',
+        message: 'What the status of this project?',
+        name: 'Status'
+    },
+    ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
