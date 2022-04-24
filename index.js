06a00d1e7af57eecc9cf7db64893432668de4cf0
yomiodusi <99558274+yomiodusi@users.noreply.github.com>
2022-04-24 12:56:38 -0400

    wrote functions

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require('fs');
+const util = require("util");
+
+const generatorMarkdown = require('./util/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [{
@@ -53,10 +58,29 @@ const questions = [{
     ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+    fs.writeFile(fileName, data, function(err) {
+        console.log(fileName)
+        console.log(data)
+        if (err) {
+            return console.log(err)
+        } else {
+            console.log("success")
+        }
+    })
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+        .then(function(data) {
+            writeToFile("README.md", generatorMarkdown(data));
+            console.log(data)
+
+        })
+}
 
 // Function call to initialize app
 init();
